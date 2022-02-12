8f915ad8cb6ba46aa54e0de6b3ec432bd63ad4c2
heavyrisem <pyo1748@gmail.com>
2022-02-12 19:28:27 +0900

    dev: 웹훅 API 추가

--- src/github/webhook/webhook.controller.ts
+++ src/github/webhook/webhook.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { GithubGuard, GithubWebhookEvents } from '@dev-thought/nestjs-github-webhooks';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 
 @Controller('webhook')
-export class WebhookController {}
+export class WebhookController {
+    @UseGuards(GithubGuard)
+    @GithubWebhookEvents(['push'])
+    @Post()
+    withRestrictedGithubEvents(@Body() payload) {
+        console.log(payload);
+    }
+}
--- src/github/webhook/webhook.module.ts
+++ src/github/webhook/webhook.module.ts
@@ -1,8 +1,14 @@
+import { GithubWebhooksModule } from '@dev-thought/nestjs-github-webhooks';
 import { Module } from '@nestjs/common';
 import { WebhookController } from './webhook.controller';
 import { WebhookService } from './webhook.service';
 
 @Module({
+    imports: [
+        GithubWebhooksModule.forRoot({
+            webhookSecret: 'SECRETDATA',
+        }),
+    ],
     controllers: [WebhookController],
     providers: [WebhookService],
 })
