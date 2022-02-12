f7f846f20c5eccd4037cef1afe13a725ffcd9f9a
heavyrisem <pyo1748@gmail.com>
2022-02-12 20:34:21 +0900

    dev: env json string 검증 함수 추가

--- src/config.module.ts
+++ src/config.module.ts
@@ -2,6 +2,29 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import * as Joi from 'joi';
 
+const GIT_REPOS_SCHEMA = Joi.custom((value, helpers) => {
+    let object: string;
+    try {
+        object = JSON.parse(value);
+    } catch (err) {
+        throw new Error('Falid to Parse JSON env');
+    }
+
+    const schema = Joi.array()
+        .items(
+            Joi.object().keys({
+                URL: Joi.string().required(),
+                BRANCH: Joi.string().required(),
+            }),
+        )
+        .required();
+    const result = schema.validate(object);
+
+    if (result.error) {
+        throw result.error;
+    }
+    return object;
+}).required();
 @Module({
     imports: [
         ConfigModule.forRoot({
@@ -9,7 +32,8 @@ import * as Joi from 'joi';
             envFilePath: '.env',
             validationSchema: Joi.object({
                 NODE_ENV: Joi.string().valid('dev', 'prod', 'test').required(),
-                PORT: Joi.number(),
+                PORT: Joi.number().default(3000),
+                GIT_REPOS: GIT_REPOS_SCHEMA,
             }),
         }),
     ],
