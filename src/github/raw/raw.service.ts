fc2ba36b112f0022fc1175cb3a97f0da468bf5fc
heavyrisem <pyo1748@gmail.com>
2022-02-13 21:04:10 +0900

    fix: 경로 입력 문제 수정

--- src/github/raw/raw.service.ts
+++ src/github/raw/raw.service.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import * as fs from 'fs/promises';
-import * as Path from 'path';
+import { normalize, resolve } from 'path';
 
 @Injectable()
 export class RawService {
     readFile(path: string): Promise<string> {
-        return fs.readFile(Path.normalize(Path.resolve(process.env.GIT_ROOT) + path)).then((res) => res.toString());
+        return fs.readFile(resolve(process.env.GIT_ROOT, normalize(path))).then((res) => res.toString());
     }
 }
